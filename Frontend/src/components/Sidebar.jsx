710e8a62b9ad06c6b5a02ba5bde6e06e12cac527
Ramamurthy <naveen.ramamurthy@accenture.com>
2026-05-27 02:34:18 +0530

    UI completed

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
-import assets from "../assets/assets";
+import React, { useEffect } from "react";
+import assets, { userDummyData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
-const Sidebar = ({ selectedUser }) => {
+const Sidebar = ({ selectedUser, setSelectedUser }) => {
   const navigate = useNavigate();
+  useEffect(() => {
+    console.log(selectedUser);
+  }, [selectedUser]);
   return (
     <div
       className={`bg-[#818582]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ""}`}
@@ -27,6 +30,42 @@ const Sidebar = ({ selectedUser }) => {
               <p className="cursor-pointer text-sm">Logout</p>
             </div>
           </div>
+        </div>
+        <div className="bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5">
+          <img src={assets.search_icon} alt="search" className="w-3" />
+          <input
+            type="text"
+            className="bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1"
+            placeholder="Search User..."
+          />
+        </div>
+        <div className="flex flex-col mt-5 h-50 overflow-y-scroll">
+          {userDummyData.map((user, index) => (
+            <div
+              onClick={() => setSelectedUser(user)}
+              key={index}
+              className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && "bg-[#282142]/50"}`}
+            >
+              <img
+                src={user?.profilePic || assets.avatar_icon}
+                alt=""
+                className="w-8.75 aspect-square rounded-full"
+              />
+              <div className="flex flex-col leading-5">
+                <p>{user.fullName}</p>
+                {index < 3 ? (
+                  <span className="text-green-400 text-xs">Online</span>
+                ) : (
+                  <span className="text-neutral-400 text-xs"> Offline</span>
+                )}
+              </div>
+              {index > 2 && (
+                <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+                  {index}
+                </p>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </div>
