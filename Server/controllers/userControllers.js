20aa94f96e1204cda14cdab83022eb693b206079
Ramamurthy <naveen.ramamurthy@accenture.com>
2026-05-28 15:55:12 +0530

    Backend Implementation - User API's

---
+++ Server/controllers/userControllers.js
@@ -0,0 +1,65 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.js";
+import { express } from "express";
+
+export const signUp = async () => {
+  const { fullName, email, password, bio } = req.body;
+  try {
+    if (!fullName || !email || !password || !bio) {
+      return res.json({ success: false, message: "Missing details" });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.json({ success: false, message: "Account already exist" });
+    }
+    const salt = await genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+      bio,
+    });
+
+    const token = generateToken(newUser._id);
+    res.json({
+      success: true,
+      userData: newUser,
+      token,
+      message: "Account created successfullly",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//conroller to login user
+export const login = async () => {
+  try {
+    const { email, password, bio } = req.body;
+    const userData = await User.findOne({ email });
+
+    const isPassword = await bcrypt.compare(userData.password);
+
+    if (!isPassword) {
+      return res.json({ success: false, message: "Invalid Credentials" });
+    }
+    const token = generateToken(userData._id);
+    return res.json({
+      success: true,
+      userData,
+      token,
+      message: "Login successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
